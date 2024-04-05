69a1471bbdf9666162be2fad489c8efdf79e27ae
Antoine <antoine.mulot2005@gmail.com>
2024-04-06 00:02:16 +0200

    fix for new recipe format

--- Classes.cs
+++ Classes.cs
@@ -1,28 +1,106 @@
+using Newtonsoft.Json;
 namespace BitCraftulator;
 
-public class Ingredient
-{
-    public string Tier { get; set; }
-    public string Name { get; set; }
-    public int Quantity { get; set; }
+public class Ingredient{
+     private string? _name;
+
+     [JsonProperty("Name")]
+     public string? Name {
+          get => _name;
+          set => _name = value ?? throw new ArgumentException("Ingredient Name is required");
+     }
+
+     [JsonProperty("Tier")]
+     public int Tier { get; set; }
+
+     [JsonProperty("Quantity")]
+     public int Quantity { get; set; }
+
+     public override string ToString() => $"[Tier {Tier}] {Name}: {Quantity}";
 }
 
-public class Output
-{
-    public string Tier { get; set; }
-    public string Name { get; set; }
-    public int Quantity { get; set; }
+public class Output{
+     private string? _name;
+
+     [JsonProperty("Name")]
+     public string? Name {
+          get => _name;
+          set => _name = value ?? throw new ArgumentException("Output Name is required");
+     }
+     [JsonProperty("Tier")]
+     public int Tier { get; set; }
+
+     [JsonProperty("Quantity")]
+     public int Quantity { get; set; }
+     
+     public override string ToString() => $"[Tier {Tier}] {Name}: {Quantity}";
 }
 
-public class Recipe
-{
-    public string RecipeName { get; set; }
-    public string Tier { get; set; }
-    public string StationName { get; set; }
-    public string SkillName { get; set; }
-    public int SkillLevel { get; set; }
-    public string ToolName { get; set; }
-    public int Effort { get; set; }
-    public List<Ingredient> Ingredients { get; set; }
-    public List<Output> Output { get; set; }
+public class Recipe{
+     private string? _recipeName;
+     private string? _skillName;
+     private string? _skillTool;
+     private string? _station;
+     private List<Ingredient>? _ingredients;
+     private List<Output>? _output;
+
+
+     [JsonProperty("RecipeName")]
+     public string? RecipeName {
+          get => _recipeName;
+          set => _recipeName = value ?? throw new ArgumentException("Recipe Name is required");
+     }
+
+     [JsonProperty("Tier")]
+     public int Tier { get; set; }
+
+     [JsonProperty("SkillName")]
+     public string? SkillName {
+          get => _skillName;
+          set => _skillName = value ?? throw new ArgumentException("Skill Name is required");
+     }
+
+     [JsonProperty("SkillLevel")]
+     public int SkillLevel { get; set; }
+
+     [JsonProperty("SkillTool")]
+     public string? SkillTool {
+          get => _skillTool;
+          set => _skillTool = value ?? throw new ArgumentException("Skill Tool is required");
+     }
+
+     [JsonProperty("Station")]
+     public string? Station {
+          get => _station;
+          set => _station = value ?? throw new ArgumentException("Station is required");
+     }
+
+     [JsonProperty("Effort")]
+     public int Effort { get; set; }
+
+     [JsonProperty("Ingredients")]
+     public List<Ingredient>? Ingredients {
+          get => _ingredients;
+          set => _ingredients = value ?? throw new ArgumentException("Ingredients are required");
+     }
+
+     [JsonProperty("Output")]
+     public List<Output>? Output {
+          get => _output;
+          set => _output = value ?? throw new ArgumentException("Output is required");
+     }
+
+     public override string ToString()
+     {
+          string res = $"['{RecipeName}'] :\r\n\tTier: {Tier}\r\n\tSkill: {SkillName}\r\n\t\tLevel: {SkillLevel}\r\n\t\tTool: {SkillTool}\r\n\t\tStation: {Station}\r\n\tIngredient{(Ingredients!.Count>1?"s":"")}: ({Ingredients!.Count})\r\n";
+
+          foreach (Ingredient ingredient in Ingredients)
+               res += $"\t\t{ingredient}\r\n";
+
+          res += $"\tOutput{(Output!.Count>1?"s":"")}: ({Output!.Count})\r\n";
+          foreach (Output output in Output)
+               res += $"\t\t{output}\r\n";
+
+          return res + "\r\n";
+     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,70 +30,62 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
+        // init the recipes dictionaries
         RecipesByTier = new Dictionary<string, Recipe>[6];
         Recipes = new();
         for (var i = 0; i < 6; RecipesByTier[i++] = new Dictionary<string, Recipe>()){}
         
+        // fills them up with the recipes
         var recipes = JsonConvert.DeserializeObject<List<Recipe>>(File.ReadAllText("Recipes.json"));
         foreach (var recipe in recipes!)
         {
-            // RecipesByTier
-            RecipesByTier[to_int(recipe.Tier) - 1][recipe.RecipeName] = recipe;
-            
-            // Recipes
-            Recipes[recipe.RecipeName] = recipe;
+            RecipesByTier[recipe.Tier-1][recipe.RecipeName!] = recipe;
+            Recipes[recipe.RecipeName!] = recipe;
         }
     
-        
-            
         PrintRecipes("I");
         DisplayRecipes();
         
-        
-        // Attach click event handlers to the buttons
+        // Attach click event handlers to the tier selection buttons
         I.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
         II.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
         III.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
         IV.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
         V.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
         VI.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        All.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
     }
     
     public int to_int(string input) => input switch {"I"=>1,"II"=>2,"III"=>3,"IV"=>4,"V"=>5,"VI"=>6,_=>throw new NotImplementedException()};
+    public string to_tier(int input) => input switch {1=>"I",2=>"II",3=>"III",4=>"IV",5=>"V",6=>"VI",_=>throw new NotImplementedException()};
 
     public void DisplayRecipeIngredients(string recipeName)
     {
         
         Console.WriteLine("1");
-        // Create and show the new window
-        RecipeInfo newWindow = new RecipeInfo(Recipes[recipeName.Replace("_"," ")]);
-        Console.WriteLine("11");
-        newWindow.Show();
-        Console.WriteLine("1111");
     }
     
     public void DisplayRecipes(string tier = "All")
     {
         Console.WriteLine($"display ingredients of tier: {tier}");
-        RecipesGrid.Children.Clear();
+        //RecipesGrid.Children.Clear();
 
-        var i = 0;
-        foreach (var (recipe, ingredients) in tier is "All" ? Recipes : RecipesByTier[to_int(tier) - 1])
+        var i = RecipesGrid.Children.Count;
+        foreach (var (recipeName, recipe) in tier is "All" ? Recipes : RecipesByTier[to_int(tier) - 1])
         {
             // Main element to add
             var elemToAdd = new Border
             {
                 Margin = new Thickness(5),
                 BorderThickness = new Thickness(1),
-                BorderBrush = Brushes.Black,
-                CornerRadius = new CornerRadius(10)
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(10),
+                Background = Brushes.LightCyan
             };
             
             // The Button inside
             var button = new Button
             {
-                Name = recipe.Replace(" ", "_"),
+                Name = recipeName.Replace(" ", "_"),
                 Margin = new Thickness(5), 
                 Background = Brushes.Transparent, 
                 BorderBrush = Brushes.Transparent
@@ -104,16 +96,16 @@ public partial class MainWindow : Window
             
             // The Image and Label inside the StackPanel
             var image = new Image {Source = new BitmapImage {UriSource = new Uri("pack://application:,,,/Images/download.png")}};
-            var label = new Label {Content = recipe};
+            var textBlock = new TextBlock {Text = recipeName, TextWrapping = TextWrapping.Wrap};
             
             // Construct the final element to add
             stackPanel.Children.Add(image);
-            stackPanel.Children.Add(label);
+            stackPanel.Children.Add(textBlock);
             button.Content = stackPanel;
             elemToAdd.Child = button;
             
-            elemToAdd.SetValue(Grid.ColumnProperty, i%6);
-            elemToAdd.SetValue(Grid.RowProperty, i/6);
+            elemToAdd.SetValue(Grid.ColumnProperty, i%5);
+            elemToAdd.SetValue(Grid.RowProperty, i/5);
             
             RecipesGrid.Children.Add(elemToAdd);
             button.Click += (sender, e) => DisplayRecipeIngredients(((Button)sender).Name);
@@ -122,23 +114,10 @@ public partial class MainWindow : Window
         }
     }
     
-    public void PrintRecipes(string tier)
+    public void PrintRecipes(string tier = "All")
     {
-        foreach (Recipe recipe in RecipesByTier[to_int(tier)-1].Values)
-        {
-            Console.WriteLine($"['{recipe.RecipeName}'] :");
-            Console.WriteLine();
-            
-            Console.WriteLine($"Ingredient{(recipe.Ingredients.Count > 0 ? "s" : "")}:");
-            foreach (Ingredient ingredient in recipe.Ingredients)
-                Console.WriteLine($"|\t{ingredient}");
-            
-            Console.WriteLine($"Output{(recipe.Output.Count > 0 ? "s" : "")}:");
-            foreach (Output output in recipe.Output)
-                Console.WriteLine($"|\t{output}");
-            
-            Console.WriteLine();
-        }
+        foreach (Recipe recipe in (tier is "All" ? Recipes.Values : RecipesByTier[to_int(tier)-1].Values))
+            Console.WriteLine(recipe);
     }
     private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
     {
