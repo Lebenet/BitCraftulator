396255c44ab8367dbf4f363f1f487bf972547b14
Antoine <antoine.mulot2005@gmail.com>
2024-04-06 17:53:57 +0200

    fix

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System.IO;
-using System.Net;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Newtonsoft.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace BitCraftulator;
 
