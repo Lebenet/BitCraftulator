1d1ea0af4783bab8b544f937be22f90d0771cd14
Antoine <antoine.mulot2005@gmail.com>
2024-04-06 16:05:25 +0200

    functionning app, need to add all recipes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,43 +26,164 @@ public partial class MainWindow : Window
     private Dictionary<string, Recipe>[] RecipesByTier { get; set; }
     private Dictionary<string, Recipe> Recipes { get; set; }
     
+    private Dictionary<string, Recipe>[] ItemsByTier { get; set; }
+    private Dictionary<string, Recipe> Items { get; set; }
+
+    private string?[] Tiers { get; set; }
+    
     public MainWindow()
     {
         InitializeComponent();
 
+        Tiers = new[] { "I", "II", "III", "IV", "V", "VI" };
+        
         // init the recipes dictionaries
         RecipesByTier = new Dictionary<string, Recipe>[6];
         Recipes = new();
-        for (var i = 0; i < 6; RecipesByTier[i++] = new Dictionary<string, Recipe>()){}
+        ItemsByTier = new Dictionary<string, Recipe>[6];
+        Items = new();
+        
+        for (var i = 0; i < 6; (RecipesByTier[i], ItemsByTier[i++]) = (new Dictionary<string, Recipe>(), new Dictionary<string, Recipe>())){}
         
         // fills them up with the recipes
         var recipes = JsonConvert.DeserializeObject<List<Recipe>>(File.ReadAllText("Recipes.json"));
         foreach (var recipe in recipes!)
         {
-            RecipesByTier[recipe.Tier-1][recipe.RecipeName!] = recipe;
-            Recipes[recipe.RecipeName!] = recipe;
+            if (recipe.Ingredients!.Count > 0)
+            {
+                RecipesByTier[recipe.Tier-1][recipe.RecipeName!] = recipe;
+                Recipes[recipe.RecipeName!] = recipe;
+            }
+            else
+            {
+                ItemsByTier[recipe.Tier-1][recipe.RecipeName!] = recipe;
+                Items[recipe.RecipeName!] = recipe;
+            }
         }
     
         PrintRecipes("I");
         DisplayRecipes();
         
         // Attach click event handlers to the tier selection buttons
-        I.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        II.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        III.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        IV.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        V.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
-        VI.Click += (sender, e) => DisplayRecipes(((Button)sender).Name);
+        I.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            I.Background = I.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
+        II.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            II.Background = II.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
+        III.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            III.Background = III.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
+        IV.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            IV.Background = IV.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
+        V.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            V.Background = V.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
+        VI.Click += (sender, e) =>
+        {
+            string tier = ((Button)sender).Name;
+            Tiers[to_int(tier) - 1] = Tiers[to_int(tier) - 1] is null ? tier: null;
+            VI.Background = VI.Background == Brushes.Chartreuse ? Brushes.LightGray : Brushes.Chartreuse;
+            DisplayRecipes();
+        };
         SearchBar.PreviewMouseDown += (sender, e) =>
         {
             if (!SearchBar.IsFocused) SearchBar.Text = "";
         };
         Close.Click += (sender, e) => Close();
+        Steps.Click += (sender, e) => DisplaySteps(((string)RecipeName.Content).Replace("_", " "));
     }
     
     public int to_int(string input) => input switch {"I"=>1,"II"=>2,"III"=>3,"IV"=>4,"V"=>5,"VI"=>6,_=>throw new NotImplementedException()};
     public string to_tier(int input) => input switch {1=>"I",2=>"II",3=>"III",4=>"IV",5=>"V",6=>"VI",_=>throw new NotImplementedException()};
 
+    public void DisplaySteps(string recipeName)
+    {
+        if (recipeName is "Recipe Name")
+            return;
+        
+        Stack<List<Recipe>> steps = new();
+        Recipe recipe = Recipes[recipeName];
+        Queue<Recipe?> q = new Queue<Recipe?>();
+        
+        steps.Push(new List<Recipe> {recipe});
+        q.Enqueue(recipe);
+        q.Enqueue(null);
+
+        List<Recipe> build = new();
+        while (q.Count > 0)
+        {
+            Recipe? curr = q.Dequeue();
+            
+            if (curr is null)
+            {
+                if (build.Count > 0)
+                    steps.Push(build);
+                build = new();
+                if (q.Count > 0)
+                    q.Enqueue(null);
+            }
+            
+            else
+            {
+                foreach (var ingredient in curr.Ingredients!)
+                {
+                    for (var i = 0; i < ingredient.Quantity; i++)
+                    {
+                        Recipe ingr;
+                        try
+                        {
+                            ingr = Recipes[ingredient.Name!];
+                        }
+                        catch (KeyNotFoundException)
+                        {
+                            ingr = Items[ingredient.Name!];
+                        }
+                        build.Add(ingr);
+                        q.Enqueue(ingr);
+                    }
+                }
+            }
+        }
+
+        var stepsWindow = new ShowSteps(steps);
+        stepsWindow.Show();
+        //PrintSteps(steps);
+    }
+
+    public void PrintSteps(Stack<List<Recipe>> steps)
+    {
+        while (steps.Count > 0)
+        {
+            var step = steps.Pop();
+            foreach (var recipe in step)
+            {
+                Console.WriteLine(recipe);
+            }
+        }
+    }
+    
     public void DisplayRecipeIngredients(string recipeName)
     {
 
@@ -79,7 +200,7 @@ public partial class MainWindow : Window
         foreach (var ingredient in recipe.Ingredients!)
         {
             Ingredients.RowDefinitions.Add(new RowDefinition());
-            Label label = new Label { Content = $"[Tier {ingredient.Tier}] {ingredient.Name}: {ingredient.Tier}" };
+            Label label = new Label { Content = ingredient.ToString(), HorizontalAlignment = HorizontalAlignment.Center};
             label.SetValue(Grid.RowProperty, Ingredients.Children.Count);
             Ingredients.Children.Add(label);
         }
@@ -88,60 +209,66 @@ public partial class MainWindow : Window
         foreach (var output in recipe.Output!)
         {
             Output.RowDefinitions.Add(new RowDefinition());
-            Label label = new Label { Content = $"[Tier {output.Tier}] {output.Name}: {output.Tier}" };
+            Label label = new Label { Content = output.ToString(), HorizontalAlignment = HorizontalAlignment.Center };
             label.SetValue(Grid.RowProperty, Output.Children.Count);
             Output.Children.Add(label);
         }
     }
     
-    public void DisplayRecipes(string tier = "All")
+    public void DisplayRecipes()
     {
-        Console.WriteLine($"display ingredients of tier: {tier}");
-        //RecipesGrid.Children.Clear();
+        RecipesGrid.Children.Clear();
 
-        var i = RecipesGrid.Children.Count;
-        foreach (var (recipeName, recipe) in tier is "All" ? Recipes : RecipesByTier[to_int(tier) - 1])
+        foreach (var tier in Tiers)
         {
-            // Main element to add
-            var elemToAdd = new Border
+            if (tier is null)
+                continue;
+            
+            var i = RecipesGrid.Children.Count;
+            foreach (var (recipeName, recipe) in tier is "All" ? Recipes : RecipesByTier[to_int(tier) - 1])
             {
-                Margin = new Thickness(5),
-                BorderThickness = new Thickness(1),
-                BorderBrush = Brushes.Transparent,
-                CornerRadius = new CornerRadius(10),
-                Background = Brushes.LightCyan
-            };
+                // Main element to add
+                var elemToAdd = new Border
+                {
+                    Margin = new Thickness(5),
+                    BorderThickness = new Thickness(1),
+                    BorderBrush = Brushes.Transparent,
+                    CornerRadius = new CornerRadius(10),
+                    Background = Brushes.LightCyan
+                };
             
-            // The Button inside
-            var button = new Button
-            {
-                Name = recipeName.Replace(" ", "_"),
-                Margin = new Thickness(5), 
-                Background = Brushes.Transparent, 
-                BorderBrush = Brushes.Transparent
-            };
+                // The Button inside
+                var button = new Button
+                {
+                    Name = recipeName.Replace(" ", "_"),
+                    Margin = new Thickness(5), 
+                    Background = Brushes.Transparent, 
+                    BorderBrush = Brushes.Transparent
+                };
             
-            // The StackPanel inside the Button
-            var stackPanel = new StackPanel();
+                // The StackPanel inside the Button
+                var stackPanel = new StackPanel();
             
-            // The Image and Label inside the StackPanel
-            var image = new Image {Source = new BitmapImage {UriSource = new Uri("pack://application:,,,/Images/download.png")}};
-            var textBlock = new TextBlock {Text = recipeName, TextWrapping = TextWrapping.Wrap};
+                // The Image and Label inside the StackPanel
+                var image = new Image {Source = new BitmapImage {UriSource = new Uri("pack://application:,,,/Images/download.png")}};
+                var textBlock = new TextBlock {Text = recipeName, TextWrapping = TextWrapping.Wrap};
             
-            // Construct the final element to add
-            stackPanel.Children.Add(image);
-            stackPanel.Children.Add(textBlock);
-            button.Content = stackPanel;
-            elemToAdd.Child = button;
+                // Construct the final element to add
+                stackPanel.Children.Add(image);
+                stackPanel.Children.Add(textBlock);
+                button.Content = stackPanel;
+                elemToAdd.Child = button;
             
-            elemToAdd.SetValue(Grid.ColumnProperty, i%5);
-            elemToAdd.SetValue(Grid.RowProperty, i/5);
+                elemToAdd.SetValue(Grid.ColumnProperty, i%5);
+                elemToAdd.SetValue(Grid.RowProperty, i/5);
             
-            RecipesGrid.Children.Add(elemToAdd);
-            button.Click += (sender, e) => DisplayRecipeIngredients(((Button)sender).Name);
+                RecipesGrid.Children.Add(elemToAdd);
+                button.Click += (sender, e) => DisplayRecipeIngredients(((Button)sender).Name.Replace("_"," "));
             
-            i++;
+                i++;
+            }
         }
+        
     }
     
     public void PrintRecipes(string tier = "All")
---
+++ ShowSteps.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics.Metrics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace BitCraftulator;
+
+public partial class ShowSteps : Window
+{
+    public ShowSteps(Stack<List<Recipe>> steps)
+    {
+        InitializeComponent();
+
+        var i = 0;
+        while (steps.Count > 0)
+            DisplayStep(steps.Pop(), i++);
+        
+        Close.Click += (sender, e) => Close();
+        Console.WriteLine(StepsGrid.Children.Count);
+    }
+
+    private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+    {
+        
+        Window window = Window.GetWindow((DependencyObject)sender);
+        if (window != null)
+        {
+            window.Left += e.HorizontalChange;
+            window.Top += e.VerticalChange;
+        }
+    }
+
+    public void DisplayStep(List<Recipe> stepRaw, int stepNumber)
+    {
+        Dictionary<Recipe, int> step = CleanStep(stepRaw);
+        
+        StepsGrid.RowDefinitions.Add(new RowDefinition {Height = new GridLength()});
+        Border border = new Border { Background = Brushes.Azure, BorderBrush = Brushes.Gray, BorderThickness = new Thickness(0, 3, 0, 0)};
+        border.SetValue(Grid.RowProperty, StepsGrid.Children.Count);
+        StepsGrid.Children.Add(border);
+        
+        Grid elemsGrid = new Grid {Name = "Step1"};
+        for (var i = 0; i <= step.Count; i++) elemsGrid.RowDefinitions.Add(new RowDefinition());
+        border.Child = elemsGrid;
+
+        Label stepLabel = new Label { Content = $"Step {stepNumber + 1}" };
+        elemsGrid.Children.Add(stepLabel);
+
+        foreach (var elem in step.Keys)
+        {
+            TextBlock line = new TextBlock { TextWrapping = TextWrapping.Wrap };
+            line.SetValue(Grid.RowProperty, elemsGrid.Children.Count);
+            if (elem.Ingredients!.Count == 0)
+                line.Text = $"Get {step[elem]} {elem.RecipeName}";
+            else
+                line.Text = $"Craft {step[elem]} {elem.RecipeName} at {elem.Station} with tool {elem.SkillTool} (required level {elem.SkillLevel} minimum in {elem.SkillName} skill)";
+            elemsGrid.Children.Add(line);
+        }
+
+    }
+
+    public Dictionary<Recipe, int> CleanStep(List<Recipe> stepRaw)
+    {
+        var res = new Dictionary<Recipe, int>();
+
+        foreach (var elem in stepRaw)
+        {
+            if (Contains(res, elem))
+                res[elem]++;
+            else
+                res[elem] = 1;
+        }
+
+        return res;
+    }
+
+    public bool Contains(Dictionary<Recipe, int> dico, Recipe check)
+    {
+        foreach (var elem in dico.Keys)
+            if (elem.RecipeName == check.RecipeName)
+                return true;
+        return false;
+    }
+}
